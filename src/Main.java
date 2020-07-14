005c7442c1d7ce17555bcff582c3d8ee97f16a73
omid-joukar <58634223+omid-joukar@users.noreply.github.com>
2020-07-14 08:57:34 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * Created by KPS on 7/14/2020.
+ */
+public class Main {
+    public static void main(String[] Strings) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Input a degree in Fahrenheit: ");
+        double fahrenheit = input.nextDouble();
+
+        double  celsius =(( 5 *(fahrenheit - 32.0)) / 9.0);
+        System.out.println(fahrenheit + " degree Fahrenheit is equal to " + celsius + " in Celsius");
+    }
+}
